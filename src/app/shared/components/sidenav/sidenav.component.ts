d1354b3e28aacbd4fddc335555e1b43502bd2b44
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2021-10-07 15:44:36 -0500

    [ADD] sidenav design

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { IUser } from 'app/main/interfaces/i-user';
 import { AppStateWithUser } from 'app/main/store/userStore/reducers';
@@ -9,13 +9,17 @@ import { Subscription } from 'rxjs';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
   user!: IUser | null;
   sub!: Subscription;
   constructor(private store: Store<AppStateWithUser>) { }
 
   ngOnInit(): void {
     this.sub = this.store.select('user').subscribe((data) => this.user = data.user);
+  }
+
+  ngOnDestroy(): void{
+    this.sub.unsubscribe();
   }
 
 }
